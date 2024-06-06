731dace59c036359282f800af1c0595c1d261716
artpupser <artpupser@yandex.ru>
2024-06-07 01:04:51 +0300

    hot fix
    <CR>

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -1,9 +1,9 @@
 vim.g.mapleader = " "
 --Base
-vim.keymap.set('n', '<C-s>', ':w <CR>')
-vim.keymap.set('i', '<C-s>', '<CMD>:w <CR>')
-vim.keymap.set('i', '<C-z>', '<CMD>:undo <CR>')
-vim.keymap.set('i', '<C-S-z>', '<CMD>:rendo <CR>')
+vim.keymap.set('n', '<C-s>', ':w<CR>')
+vim.keymap.set('i', '<C-s>', '<CMD>:w<CR>')
+vim.keymap.set('i', '<C-z>', '<CMD>:undo<CR>')
+vim.keymap.set('i', '<C-S-z>', '<CMD>:rendo<CR>')
 --NeoTree
 vim.keymap.set('n', '<leader>t', ':Neotree focus<CR>')
 vim.keymap.set('n','<leader>o',':Neotree git_status<CR>')
