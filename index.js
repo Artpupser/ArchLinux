e66969128f2d5bc8f1d41ac1c46c48ce4da33711
artpupser <artpupser@yandex.ru>
2025-01-15 18:30:52 +0300

    v1.5

--- index.js
+++ index.js
@@ -1,105 +1,120 @@
 let buildingInterface = [{
-   title: "Start",
-   list: [
-      "pacman -S git",
-      "cd ~/",
-      "git clone https://aur.archlinux.org/yay.git",
-      "cd ~/yay",
-      "makepkg -sric",
-      "cd ..",
-      "yay",
-      "pacman -Suy ",
-   ]
+  title: "Start",
+  list: [
+    "pacman -S git",
+    "cd ~/",
+    "git clone https://aur.archlinux.org/yay.git",
+    "cd ~/yay",
+    "makepkg -sric",
+    "cd ..",
+    "yay",
+    "pacman -Suy ",
+  ]
 },
 {
-   title: "Zsh",
-   list: [
-      "yay -S zsh",
-      "sh -c \"$(curl -fsSL https://install.ohmyz.sh/)\"",
-      "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k",
-      "nano ~/.zshrc",
-      "find parameter > ZSH_THEME > put this > \"powerlevel10k/powerlevel10k\"",
-      "reboot terminal",
-   ]
+  title: "Zsh",
+  list: [
+    "yay -S zsh",
+    "sh -c \"$(curl -fsSL https://install.ohmyz.sh/)\"",
+    "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k",
+    "nano ~/.zshrc",
+    "find parameter > ZSH_THEME > put this > \"powerlevel10k/powerlevel10k\"",
+    "reboot terminal",
+  ]
 },
 {
-   title: "Optimization",
-   list: [
-      "sudo pacman -Rns $(pacman -Qeq gnome-software gnome-calendar gnome-calls gnome-characters gnome-chess gnome-clocks gnome-contacts gnome-maps gnome-music gnome-notes gnome-photos gnome-remote-desktop gnome-text-editor gnome-tour gnome-user-docs gnome-font-viewer gnome-weather totem sushi epiphany) ",
-   ]
+  title: "Optimization",
+  list: [
+    "sudo pacman -Rns $(pacman -Qeq gnome-software gnome-calendar gnome-calls gnome-characters gnome-chess gnome-clocks gnome-contacts gnome-maps gnome-music gnome-notes gnome-photos gnome-remote-desktop gnome-text-editor gnome-tour gnome-user-docs gnome-font-viewer gnome-weather totem sushi epiphany) ",
+  ]
 },
 {
-   title: "Drivers",
-   list: [
-      "yay -S nvidia nvidia-utils nvidia-settings opencl-nvidia mesa mesa-utils"
-   ]
+  title: "Drivers",
+  list: [
+    "yay -S nvidia nvidia-utils nvidia-settings opencl-nvidia mesa mesa-utils"
+  ]
 },
 {
-   title: "Programs",
-   list: [
-      "yay -S libreoffice qbittorrent obsidian firefox vlc telegram-desktop visual-studio-code-bin blender krita"
-   ]
+  title: "Programs",
+  list: [
+    "yay -S libreoffice qbittorrent obsidian firefox vlc telegram-desktop visual-studio-code-bin blender krita"
+  ]
 },
 {
-   title:"Core",
-   list: [
-      "yay -S dotnet-sdk dotnet-runtime python lrzip unrar unzip unace p7zip squashfs-tools",
-   ]
+  title: "Core",
+  list: [
+    "yay -S dotnet-sdk dotnet-runtime python lrzip unrar unzip unace p7zip squashfs-tools",
+  ]
 },
 {
-   title:"Gaming",
-   list: [
-      "yay -S heroic-games-launcher-bin wine startwine lutris gamescope mangohud gamemode polymc",
-   ]
+  title: "Gaming",
+  list: [
+    "yay -S heroic-games-launcher-bin wine startwine lutris gamescope mangohud gamemode polymc",
+  ]
+},
+{
+  title: "Optimization",
+  list: [
+    "git clone https://aur.archlinux.org/ananicy.git",
+    "cd ananicy",
+    "makepkg -sric",
+    "sudo systemctl enable --now ananicy",
+    "sudo systemctl enable fstrim.timer",
+    "sudo fstrim -v /",
+    "sudo fstrim -va /",
+    "sudo pacman -S dbus-broker",
+    "sudo systemctl enable --now dbus-broker.service"
+
+  ]
 }]
 
 function build(building) {
-   console.log("Start buildinig...");
-   let container = getContainer();
-   if (container == null || container == undefined) {
-      createContainer();
-      container = getContainer();
-   }
-   for (const i in building){
-      const brick = building[i]
-      let a = document.createElement("a")
-      a.innerHTML = brick.title
-      a.setAttribute("href", `#${brick.title}`)
-      container.appendChild(a);
-   }
-   for (const i in building){
-      const brick = building[i]
-      let titleElement = document.createElement("h1")
-      titleElement.innerHTML = brick.title
-      titleElement.setAttribute("id", `${brick.title}`)
-      let listElement = document.createElement("ul")
-      listElement.setAttribute("class", "list")
-      for (const j in brick.list) {
-         let liElement = document.createElement("li")
-         liElement.innerHTML = brick.list[j]
-         listElement.appendChild(liElement)
-      }
-      container.appendChild(titleElement)
-      container.appendChild(listElement)
-   }
+  console.log("Start buildinig...");
+  let container = getContainer();
+  if (container == null || container == undefined) {
+    createContainer();
+    container = getContainer();
+  }
+  for (const i in building) {
+    const brick = building[i]
+    let a = document.createElement("a")
+    a.innerHTML = brick.title
+    a.setAttribute("href", `#${brick.title}`)
+    container.appendChild(a);
+  }
+  for (const i in building) {
+    const brick = building[i]
+    let titleElement = document.createElement("h1")
+    titleElement.innerHTML = brick.title
+    titleElement.setAttribute("id", `${brick.title}`)
+    let listElement = document.createElement("ul")
+    listElement.setAttribute("class", "list")
+    for (const j in brick.list) {
+      let liElement = document.createElement("li")
+      liElement.innerHTML = brick.list[j]
+      listElement.appendChild(liElement)
+    }
+    container.appendChild(titleElement)
+    container.appendChild(listElement)
+  }
 }
 
 function getContainer() {
-   return document.querySelector(".container")
+  return document.querySelector(".container")
 }
 
 function createContainer() {
-   let container = document.createElement("div")
-   container.setAttribute("class", "container")
-   document.body.appendChild(container)
+  let container = document.createElement("div")
+  container.setAttribute("class", "container")
+  document.body.appendChild(container)
 }
 
 function updateHeader(title) {
-   document.title = title
+  document.title = title
 }
 
 updateHeader("ArchLinux configuration!")
 build(buildingInterface)
 setTimeout(() => {
-   document.body.style.backgroundColor = "rgba(0,0,0,0.1)"
-},100)
+  document.body.style.backgroundColor = "rgba(0,0,0,0.1)"
+}, 100)
