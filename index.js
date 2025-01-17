ee3d1cc146dc503f8c01cb7a739b2b0163a6067b
artpupser <artpupser@yandex.ru>
2025-01-17 04:04:15 +0300

    v1.6b

--- index.js
+++ index.js
@@ -23,15 +23,29 @@ let buildingInterface = [{
   ]
 },
 {
-  title: "Optimization",
+  title: "Arch lite",
   list: [
     "sudo pacman -Rns $(pacman -Qeq gnome-software gnome-calendar gnome-calls gnome-characters gnome-chess gnome-clocks gnome-contacts gnome-maps gnome-music gnome-notes gnome-photos gnome-remote-desktop gnome-text-editor gnome-tour gnome-user-docs gnome-font-viewer gnome-weather totem sushi epiphany) ",
   ]
 },
 {
+  title: "Arch boost",
+  list: [
+    "git clone https://aur.archlinux.org/ananicy.git",
+    "cd ananicy",
+    "makepkg -sric",
+    "sudo systemctl enable --now ananicy",
+    "sudo systemctl enable fstrim.timer",
+    "sudo fstrim -v /",
+    "sudo fstrim -va /",
+    "sudo pacman -S dbus-broker",
+    "sudo systemctl enable --now dbus-broker.service"
+  ]
+},
+{
   title: "Drivers",
   list: [
-    "yay -S nvidia nvidia-utils nvidia-settings opencl-nvidia mesa mesa-utils"
+    "yay -S nvidia nvidia-utils nvidia-settings opencl-nvidia nvidia-settings mesa mesa-utils"
   ]
 },
 {
@@ -49,71 +63,56 @@ let buildingInterface = [{
 {
   title: "Gaming",
   list: [
-    "yay -S heroic-games-launcher-bin wine startwine lutris gamescope mangohud gamemode polymc",
+    "yay -S wine gamescope mangohud gamemode mono heroic-games-launcher-bin startwine lutris",
   ]
 },
 {
-  title: "Optimization",
+  title: "Steam game settings",
   list: [
-    "git clone https://aur.archlinux.org/ananicy.git",
-    "cd ananicy",
-    "makepkg -sric",
-    "sudo systemctl enable --now ananicy",
-    "sudo systemctl enable fstrim.timer",
-    "sudo fstrim -v /",
-    "sudo fstrim -va /",
-    "sudo pacman -S dbus-broker",
-    "sudo systemctl enable --now dbus-broker.service"
-
+    "[Cs2] proton-experimental:",
+    "[DayZ] proton-experimental: gamescope -f --force-grab-cursor -- %command%",
+    "[Grim dawn] proton-experimental:"
   ]
 }]
 
 function build(building) {
-  console.log("Start buildinig...");
-  let container = getContainer();
-  if (container == null || container == undefined) {
-    createContainer();
-    container = getContainer();
-  }
+  let container = newElement("div", ["class", "container"], null);
+  document.body.appendChild(container)
+  let linkContainer = newElement("div", ["class", "link-container"], null);
   for (const i in building) {
     const brick = building[i]
-    let a = document.createElement("a")
-    a.innerHTML = brick.title
-    a.setAttribute("href", `#${brick.title}`)
-    container.appendChild(a);
+    let link = newElement("a", ["href", `#${brick.title}`], brick.title)
+    link.setAttribute("class", "link")
+    linkContainer.appendChild(link);
   }
+  container.appendChild(linkContainer);
   for (const i in building) {
     const brick = building[i]
-    let titleElement = document.createElement("h1")
-    titleElement.innerHTML = brick.title
-    titleElement.setAttribute("id", `${brick.title}`)
-    let listElement = document.createElement("ul")
-    listElement.setAttribute("class", "list")
+    let titleElement = newElement("h1", ["id", `${brick.title}`], brick.title)
+    let listElement = newElement("div", ["class", "list"], null)
     for (const j in brick.list) {
-      let liElement = document.createElement("li")
-      liElement.innerHTML = brick.list[j]
-      listElement.appendChild(liElement)
+      let lineEl = newElement("p", ["class", "line"], brick.list[j])
+      listElement.appendChild(lineEl)
     }
     container.appendChild(titleElement)
     container.appendChild(listElement)
   }
 }
 
-function getContainer() {
-  return document.querySelector(".container")
+function getFromClassName(className) {
+  return document.getElementsByClassName(className)[0]
 }
 
-function createContainer() {
-  let container = document.createElement("div")
-  container.setAttribute("class", "container")
-  document.body.appendChild(container)
+function newElement(tag, [attribute, value], innerHtml) {
+  let el = document.createElement(tag)
+  if (innerHtml != null && innerHtml != "") {
+    el.innerHTML = innerHtml
+  }
+  el.setAttribute(attribute, value)
+  return el;
 }
 
-function updateHeader(title) {
-  document.title = title
-}
-
-updateHeader("ArchLinux configuration!")
+document.title = `ArchLinux configuration!`
 build(buildingInterface)
 setTimeout(() => {
   document.body.style.backgroundColor = "rgba(0,0,0,0.1)"
